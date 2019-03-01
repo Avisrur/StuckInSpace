9bcb68b647e39eae936e62e3395ffab3314889c5
Lidor Rosencovich <lidor.rosencovich@sisense.com>
2019-03-01 02:33:15 +0200

    Final Game

---
+++ Assets/GamingFiles/Scripts/Score2DGame.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Score2DGame : MonoBehaviour
+{
+    public GUIText scoreText;
+    public int coinValue;
+
+    private int score;
+
+    void Start()
+    {
+        UpdateScore();
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        GameStatusScript.score += 50;
+        UpdateScore();
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Bomb")
+        {
+            score -= 100;
+            UpdateScore();
+        }
+    }
+
+    void UpdateScore()
+    {
+        scoreText.text = "SCORE:\n" + GameStatusScript.score;
+    }
+}
---
+++ Assets/Score2Dd.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Score2Dd : MonoBehaviour
+{
+    public GUIText scoreText;
+    public int coinValue;
+
+    private int score;
+
+    void Start()
+    {
+        UpdateScore(0);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Bomb")
+        {
+            UpdateScore(-100);
+        }
+        if (other.tag == "Coin")
+        {
+            UpdateScore(50);
+        }
+        Destroy(other);
+    }
+
+    void UpdateScore(int score)
+    {
+        GameStatusScript.score += score;
+        scoreText.text = "SCORE:\n" + GameStatusScript.score;
+    }
+}
